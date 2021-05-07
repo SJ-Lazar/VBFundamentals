eaad24ba3a2c6bc761ac82336c82ca6949c4aabb
SJ-Lazar <78497706+SJ-Lazar@users.noreply.github.com>
2021-05-07 16:03:25 +0200

    Add project files.

---
+++ VBConsole/Program.vb
@@ -0,0 +1,16 @@
+Imports System
+
+'Container - Namespaces 
+Module Program
+
+    'Sub procedure
+    Sub Main(args As String())
+
+        'Methods From Console Class
+        Console.WriteLine("Hello Nike!")
+        Console.ReadLine()
+
+
+    End Sub
+
+End Module
